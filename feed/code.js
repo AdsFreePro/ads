b9982ea6f08e37a0b4517fe6e24a58be81fe1f8b
Everything4info <54599875+Everything4info@users.noreply.github.com>
2020-02-21 22:02:40 +0200

    Create code.js

---
+++ feed/code.js
@@ -0,0 +1,65 @@
+MySite_id = 2;
+var MySite_now = new Date();
+var detik = MySite_now.getSeconds();
+var AdsFreePro_ads1 = detik % MySite_id;
+AdsFreePro_ads1 += 1;
+if (AdsFreePro_ads1 == 1) {
+var text1="مساحة اعلانية";
+var link1="https://adsfreepro.github.io/home/";
+var img1="https://lh3.googleusercontent.com/-AtRfv1rCdUI/XlAq1nyWMkI/AAAAAAAAAQI/Foe69QR1gXYMvCkn-bWVw4iRQeFG5hjHgCLcBGAsYHQ/h120/%25D9%25A2%25D9%25A0%25D9%25A2%25D9%25A0%25D9%25A0%25D9%25A2%25D9%25A2%25D9%25A1_%25D9%25A2%25D9%25A1%25D9%25A0%25D9%25A7%25D9%25A5%25D9%25A6.jpg";
+
+}
+if (AdsFreePro_ads1 == 2) {
+var text1="مساحة اعلانية";
+var link1="https://adsfreepro.github.io/home/";
+var img1="https://lh3.googleusercontent.com/-AtRfv1rCdUI/XlAq1nyWMkI/AAAAAAAAAQI/Foe69QR1gXYMvCkn-bWVw4iRQeFG5hjHgCLcBGAsYHQ/h120/%25D9%25A2%25D9%25A0%25D9%25A2%25D9%25A0%25D9%25A0%25D9%25A2%25D9%25A2%25D9%25A1_%25D9%25A2%25D9%25A1%25D9%25A0%25D9%25A7%25D9%25A5%25D9%25A6.jpg";
+
+}
+MySite_id = 2;
+var MySite_now = new Date();
+var detik = MySite_now.getSeconds();
+var AdsFreePro_ads2 = detik % MySite_id;
+AdsFreePro_ads2 += 1;
+if (AdsFreePro_ads2 == 1) {
+var text2="تحديث مهم لمتصفح Chrome فبراير 2020";
+var link2="https://i-know-now-plus.blogspot.com/2020/02/Important-update-for-Chrome.February-2020.html";
+var img2="https://1.bp.blogspot.com/-fdnX95TR9ZU/Xk1bVfmkprI/AAAAAAAAAP8/hkt8HanLrjMDoojPA_QaAgi6nh5ynXp0gCLcBGAsYHQ/s1600/%25D9%25A2%25D9%25A0%25D9%25A2%25D9%25A0%25D9%25A0%25D9%25A2%25D9%25A1%25D9%25A9_%25D9%25A1%25D9%25A7%25D9%25A5%25D9%25A6%25D9%25A5%25D9%25A0.jpg";
+
+
+}
+if (AdsFreePro_ads2 == 2) {
+
+var text2="اسباب عدم ظهور اعلانات ادسنس";
+var link2="https://i-know-now-plus.blogspot.com/2020/01/Reasons-why-adsense-ads-do-not-appear.html";
+var img2="https://1.bp.blogspot.com/-oPZztzwkkCA/Xg-HEqVw4TI/AAAAAAAAAMY/ySrxJLJC0XItabsC2Pslx1XfHzH1Hd0VwCLcBGAsYHQ/s1600/adsense.png";
+
+}
+MySite_id = 2;
+var MySite_now = new Date();
+var detik = MySite_now.getSeconds();
+var AdsFreePro_ads3 = detik % MySite_id;
+AdsFreePro_ads3 += 1;
+if (AdsFreePro_ads3 == 1) {
+var text3="ما هي البرمجة و اساسياتها";
+var link3="https://gerges-shamoon-developer.blogspot.com/2019/12/blog-post.html";
+var img3="https://1.bp.blogspot.com/-qUBVEG5KMGw/Xej-W3GFCBI/AAAAAAAACRg/vF0lzCddqhIV1UGVpsplLqD6U6x3CEnGQCLcBGAsYHQ/s1600/%25D9%25A2%25D9%25A0%25D9%25A1%25D9%25A9%25D9%25A1%25D9%25A2%25D9%25A0%25D9%25A5_%25D9%25A1%25D9%25A4%25D9%25A1%25D9%25A2%25D9%25A5%25D9%25A6.jpg";
+
+
+}
+if (AdsFreePro_ads3 == 2) {
+
+var text3="نصائح لمدونين 2020";
+var link3="https://gerges-shamoon-developer.blogspot.com/2020/01/Tips-for-bloggers-2020.html";
+var img3="https://1.bp.blogspot.com/-5qD5G8zvEPY/XhJEolhBC9I/AAAAAAAAAMk/bxRT0wJuFGM9edIaGlVUlb2tPTOxrtgZwCLcBGAsYHQ/s1600/%25D9%25A2%25D9%25A0%25D9%25A2%25D9%25A0%25D9%25A0%25D9%25A1%25D9%25A0%25D9%25A5_%25D9%25A2%25D9%25A2%25D9%25A1%25D9%25A7%25D9%25A2%25D9%25A6.jpg";
+
+}
+
+document.write("<div id='box-AdsFreePro-AllFeed'>");
+document.write("<div id='feed'><a href='https://adsfreepro.github.io/link/??&&url=_" + link1 + " '><img src='" + img1 + "'/><div id='text'>" + text1 + "</div></a></div>");
+document.write("<div id='feed'><a href='https://adsfreepro.github.io/link/??&&url=_" + link2 + " '><img src='" + img2 + "'/><div id='text'>" + text2 + "</div></a></div>");
+document.write("<div id='feed'><a href='https://adsfreepro.github.io/link/??&&url=_" + link3 + " '><img src='" + img3 + "'/><div id='text'>" + text3 + "</div></a></div>");
+document.write("<p>By AdsFreePro</p>");
+document.write("</div>");
+//CSS Ready
+function loadCSS(e, t, n) { 'use strict'; var i = window.document.createElement('link'); var o = t || window.document.getElementsByTagName('script')[0]; i.rel = 'stylesheet'; i.href = e; i.media = 'only x'; o.parentNode.insertBefore(i, o); setTimeout(function () { i.media = n || 'all' }) }
+loadCSS('https://adsfreepro.github.io/ads/feed/style.css');
